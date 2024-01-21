ee506f12460d405eed3c42b600cfe1cf707f555e
Adithya Sunil <adithyasunil04@outlook.com>
2024-01-21 20:14:00 +0530

    added little verbose to browser installer script

--- src/scripts/browserInstaller.sh
+++ src/scripts/browserInstaller.sh
@@ -9,23 +9,33 @@ menu() {
         2>&1 >/dev/tty
 }
 
+# Made the script more verbose, for the end user.
+
 while true; do
     choice=$(menu)
     case $choice in
         1)
-            echo "Setting default browser to Chromium"
+            echo "Downloading Chromium..."
 	    yay -S chromium --noconfirm
+        echo "\n==> Downloaded and Installed Chromium."
 	    xdg-settings set default-web-browser chromium.desktop
+        echo "\nSetting Default Browser to Chromium: OK"
             ;;
         2)
-            echo "Setting default browser to Brave"
+            echo "Downloading Brave..."
 	    yay -S brave-bin --noconfirm
+        echo "\n==> Downloaded and Installed Brave."
 	    xdg-settings set default-web-browser brave-browser.desktop
+        echo "\nSetting Default Browser to Brave: OK"
+
             ;;
         3)
-            echo "Setting default browser to Librewolf"
+            echo "Downloading Librewolf..."
 	    yay -S librewolf-bin --noconfirm
+        echo "\n==> Downloaded and Installed LibreWolf."
 	    xdg-settings set default-web-browser librewolf.desktop
+        echo "\nSetting Default Browser to LibreWolf: OK"
+
             ;;
         *)
             echo "Exiting..."
